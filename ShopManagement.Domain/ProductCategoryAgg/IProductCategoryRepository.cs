179730a6f3ccdc720e5ba78ca3cfa4e1e3c85f3b
Yoosof Taher <107296042+Yoosoftaherolghalb@users.noreply.github.com>
2022-07-12 13:56:02 +0430

    CreateProductCategoryDomainAndApplicationLayer

---
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using ShopManagement.Application.Contract.ProductCategory;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+        ProductCategory Get(long id);
+        List<ProductCategory> GetAll();
+        bool Exists(Expression<Func<ProductCategory, bool>> expression);
+        void SaveChanges();
+        EditProductCategory GetDetails(long id);
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+    }
+}
